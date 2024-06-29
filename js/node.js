3b655991cc4d51afd83f28b25f898d9ef765ae64
Aj <ajtech8120@gmail.com>
2024-06-28 17:01:57 -0700

    added submit function to store user data

--- js/node.js
+++ js/node.js
@@ -7,27 +7,92 @@ import horoscopeData from "../data/data.js";
 
 let signList = horoscopeData.horoscopes.astroSigns;
 let showHorsocope = horoscopeData.horoscopes.astroSigns[0].dailyHoroscope;
-let newArray = [];
+let userArray = [];
 let sign = document.getElementById("sign");
 let showDiv = document.getElementsByClassName("show-div");
-// var selectedSign = documment.getElementsById(option);
+// let singToPin = option.textcontent
+// var yourSign = documment.getElementsById("option");
 
 
 function forEachTest() {
-    signList.forEach(item => sign.innerHTML += `<option>${item.sign}</option>`);
-    
-    console.log(sign);
-
+    signList.forEach(item => sign.innerHTML += `<option value=${item.sign}>${item.sign}</option>`);
 }
-    
 forEachTest()
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+	const  yourSign= new FormData(event.target);
+	const dataObject = Object.fromEntries(yourSign.entries());
+	//above is boiler plate code
+    userArray.push(dataObject);
+    console.log(userArray);
+    console.log(dataObject);
+	// form.reset();
+}
+function userHoroscope() {
+    for (let item of signList) {
+        if (signList.sign === userArray) {
+            document.getElementsByClassName("your-sign").textContent = signList.sign;
+        }
+    }
+}
+
+
+
+
+
+//submit function that grabs the value from the user and stores it in a variable
+//then, loop through the data and use an if statement to see if the value from the user is the same as item.sign
+//inside the code block for the if statement, (so the code that will run when the if statement evaloustes to true) reassign the value of the sign variable and the description variable to be item.sign and item.dailyHoroscope
+//display the info back to the user
+
+
+// function for reset button on form-----------------------------------------------------------
+// function onFormReset () {
+//     form.reset();
+// }
+
+//original boilerplate for the submit button-------------------------------------------------------
+// function onFormSubmit(event) {
+// 	event.preventDefault();
+// 	const data = new FormData(event.target);
+// 	const dataObject = Object.fromEntries(data.entries());
+// 	console.log(dataObject);
+// 	//above is boiler plate code
+// 	form.reset();
+// }
+
+
+// function forEachLoop(item) {
+//     return sign.innerHTML += `<option value=${item.sign}>${item.sign}</option>`
+// }
+
+// function addTwo(num1, num2) {
+//     return num1 + num2
+// }
+
+// let answer = addTwo(56, 4)
+// let answer1 = addTwo(45, 4)
+// console.log(answer)
+
+
+
+
+
+
+
+// function yourSign () {
+// for (let item of signList){
+//     let signIs = documment.getElementsById("select");
+//     console.log(signIs)
+// }
+// }
+
+// yourSign()
 // need to loop through and find array object that has same value as 
 
-function showHoroscope () {
-
-  
-}
 
 
 
