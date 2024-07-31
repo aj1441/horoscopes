fc6ecb68c52340675cb63476dc82202d2d9d1d00
aj1441 <167340937+aj1441@users.noreply.github.com>
2024-07-31 13:55:03 -0700

    added rest function to button and fianlized styling Done

--- js/node.js
+++ js/node.js
@@ -4,20 +4,8 @@ import horoscopeData from "../data/data.js";
 
 
 let signList = horoscopeData.horoscopes.astroSigns;
-// let userHoroscope = horoscopeData.horoscopes.astroSigns.dailyHoroscope
 let showHoroscope = document.getElementById("userHoroscope");
-// let showSign = document.getElementById("yourSign");
-let form= document.getElementById("form");
-// let userSign;
-let display;
-// let newUserSign = document.getElementById("userSign")
-// console.log(showHoroscope)
-// console.log(showSign)
-
-
-
-
-
+let form = document.getElementById("form");
 
 
 function forEachTest() {
@@ -29,27 +17,31 @@ form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
     event.preventDefault();
-	const data = new FormData(event.target);
-	const dataObject = Object.fromEntries(data.entries());
+    const data = new FormData(event.target);
+    const dataObject = Object.fromEntries(data.entries());
     console.log(dataObject);
-	//above is boiler plate code
     form.reset();
     let userSign = dataObject.sign;
-
     for (let item of signList) {
-        if (item.sign === userSign){
-            let selectedSign=item.sign;
+        if (item.sign === userSign) {
+            let selectedSign = item.sign;
             let selectedHoroscope = item.dailyHoroscope;
-        let horoscopeContainer =document.createElement("div");
-        horoscopeContainer.className = "horoscope-wrapper";
-        horoscopeContainer.innerHTML = `<div class="card" id="card">
+            let horoscopeContainer = document.createElement("div");
+            horoscopeContainer.className = "horoscope-wrapper";
+            horoscopeContainer.innerHTML = `<div class="card" id="card">
         <p class="selected-sign">Your sign is ${selectedSign}</p>
         <p class="selected-horoscope">Your daily horoscope is: ${selectedHoroscope}</p>`
 
-        showHoroscope.appendChild(horoscopeContainer);
+            showHoroscope.appendChild(horoscopeContainer);
         }
-    // let userHoroscope = signList[userSign].dailyHoroscope;
-	// displayHoroscope ();
-    console.log(userSign);
+        console.log(userSign);
+    }
 }
+
+const refreshButton = document.querySelector('.refresh-button');
+
+const refreshPage = () => {
+    location.reload();
 }
+
+refreshButton.addEventListener('click', refreshPage)
